55a93c2853148214efcef6628009770852f1611b
NunoF <n.fernandes.contact@gmail.com>
2026-02-17 00:14:15 +0100

    Add ConnectedLayout component and related imports
    The ConnectedLayout component is responsible for rendering the layout of the pages in the application. It includes a Navbar component and a ToastContainer component from the react-toastify library.
    
    Changes:
    - Added ConnectedLayout.tsx component
    - Added related imports and dependencies

--- src/app/(pages)/layout.tsx
+++ src/app/(pages)/layout.tsx
@@ -1,12 +1,26 @@
+"use client";
+
 import Navbar from "@/src/components/shared/Navbar";
+import { Button } from "@/src/components/ui/button";
+import { ToastContainer, toast } from "react-toastify";
 
 export default function ConnectedLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const notift = () => toast("Hello World!");
+
   return (
     <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
       <Navbar />
       {children}
     </>
